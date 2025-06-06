b8f62ab86e292ae48183ad8bc846d1e10e9a0d78
User-278 <11awong@brightoncollege.net>
2025-06-06 11:52:33 +0100

    more stuff

--- routes/home.py
+++ routes/home.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import os
 
 app = Flask(__name__, 
-            template_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates'))
+            template_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates'),
+            static_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static'))
+
+app.config['SECRET_KEY'] = '5773526bb0b13ce0c676dfde280ba345'
 
 @app.route("/home")
 @app.route("/")
