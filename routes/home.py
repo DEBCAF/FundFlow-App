a1fafb8b93f3fb2a38017b80d639f297db2f4f85
User-278 <11awong@brightoncollege.net>
2025-06-18 14:00:35 +0100

    start of db

--- routes/home.py
+++ routes/home.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 import os
 import sys
 from pathlib import Path
+from forms.RegistrationForm import RegistrationForm
+from forms.LoginForm import LoginForm
 
 project_root = str(Path(__file__).parent.parent)
 if project_root not in sys.path:
     sys.path.append(project_root)
-
-from forms.RegistrationForm import RegistrationForm
-from forms.LoginForm import LoginForm
 
 app = Flask(__name__,
             template_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates'),
             static_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static'))
 
 app.config['SECRET_KEY'] = '5773526bb0b13ce0c676dfde280ba345'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 @app.route("/home")
 @app.route("/")
